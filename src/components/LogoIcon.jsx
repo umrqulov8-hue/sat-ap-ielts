f3998eeb3152c1b38a5cf5fcfa5636b74ba2ba32
umrqulov8-hue <umrqulov8-hue@users.noreply.github.com>
2026-06-04 11:52:23 +0500

    fix: remove all extra S logos - only sidebar + home nav bar remain

--- src/components/LogoIcon.jsx
+++
@@ -1,9 +0,0 @@
-import { Link } from 'react-router-dom'
-
-export default function LogoIcon({ size = 40, className = '' }) {
-  return (
-    <Link to="/dashboard" className={`logo-fixed ${className}`} style={{ width: size, height: size }}>
-      <span style={{ fontSize: size * 0.45 }}>S</span>
-    </Link>
-  )
-}
